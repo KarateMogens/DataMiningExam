462d3b303756b0456ff7747fd00a928d77467af2
EmilHoejrup <emilhoejrup@gmail.com>
2023-11-24 15:08:21 +0100

    Add boxplot pr genre and feature by year plots in eda.py

--- eda.py
+++ eda.py
@@ -3,21 +3,45 @@
 # IMPORTS:
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 from utils import get_dfs
 
-# %%
+# define functions for plotting features by year
+
+
+def plot_features_by_year(df):
+    for feature in data_ordinal_df.columns:
+        plt.figure(figsize=(12, 8))
+        sns.lineplot(data=df, x='year', y=feature)
+        plt.title(f"{feature} over years")
+        plt.show()
+
+
+def plot_yearly_features_by_genre(df):
+    for genre in df['playlist_genre'].unique():
+        plot_features_by_year(data_df[data_df['playlist_genre'] == genre])
+
+
+def plot_features_by_year_compare_genres(df):
+    for feature in data_ordinal_df.columns:
+        plt.figure(figsize=(12, 8))
+        sns.lineplot(data=data_df, x='year', y=feature, hue='playlist_genre')
+        plt.title(f"{feature} over years")
+        plt.show()
+
+
 data_df, data_ordinal_df, holdout_df = get_dfs()
 data_df.describe()
 
-# %%
-data_ordinal_df
 # %%
 # Full plot of 2-way data combinations (and distribution for each attribute)
 sns.pairplot(data_df)
 
 # %%
+# TAKES A LONG TIME!
 sns.pairplot(data_df, hue='playlist_genre')
 # %%
+# ALSO SLOW
 sns.pairplot(data_df, hue='mode')
 # %%
 correlation_matrix = data_ordinal_df.corr()
@@ -26,17 +50,34 @@ correlation_matrix = data_ordinal_df.corr()
 correlation_matrix
 sns.heatmap(correlation_matrix, cmap='Blues')
 
-# %%
-# Questions/Hypotheses:
-# -
+# # %%
+# rock = data_df[data_df['playlist_genre'] == 'rock']
+# # %%
+# sns.pairplot(rock)
 
-# Brainstorm:
-# Supervised Learning:
-# - Decision tree / Naïve bayes til at definere genre/playlist
-# Unsupervised Learning:
-# - Clustering for at definere nye genre-grupperinger
-# -
-# Correlation Analysis:
-# - Hvilke features har størst betydning for
-# Feature reduction:
-# - PCA
+
+# %%
+# plotter alle musical features ift genre
+for feature in data_ordinal_df.columns:
+    plt.figure(figsize=(12, 8))
+    sns.boxplot(x='playlist_genre', y=feature, data=data_df)
+    plt.show()
+
+
+# %%
+
+data_df.columns
+# %%
+# create 'year' columns to be able to plot feature by year
+data_df['track_album_release_date'] = pd.to_datetime(
+    data_df['track_album_release_date'], format='ISO8601')
+data_df['year'] = data_df['track_album_release_date'].dt.year
+# %%
+plot_features_by_year(data_df)
+# %%
+# MÅSKE ER DE NÆSTE TO LIDT OVERKILL
+plot_features_by_year_compare_genres(data_df)
+# %%
+plot_yearly_features_by_genre(data_df)
+
+# %%
--- tanker.md
+++ tanker.md
@@ -3,6 +3,15 @@
 - decision tree virker til at performe virkeligt godt - det er nok noget lignende spotify selv bruger til at klassificere genre
 - regression på musical features er elendig, men nok også forventeligt - ellers kunne man måske bare lave et hit ved en simpel blanding af features (120bpm + Cdur + max loudness = monsterhit)
 
+##### plots
+
+- især EDM skiller sig ud i boxplots by genre (nok forventeligt)
+- i feature by year virker aææe features til at have markant større udsving inden midt 60'erne, hvilket giver mening ift optagelsesudstyr, professionelle producere og lyd-engineers først kommer heromkiring
+- loudness wars (https://en.wikipedia.org/wiki/Loudness_war) er helt klart en ting fra man begynder at have cd'er i 90'erne. Måske kan man også se indflydelsen fra Phil Spectors "wall of sound" i slut 60'erne? (https://en.wikipedia.org/wiki/Wall_of_Sound)
+- speechiness udvikling giver mening ift at rap bliver mainstream fra slut 80'erne
+- interessant at valence falder lige omkring finanskrisen
+- duration falder kort efter at spotify bliver lanceret (2008)
+
 ##### features
 
 - det giver mening at tempo har stor betydning - jeg forestiller mig at 99.9% af alt edm har en bpm på precis 120
