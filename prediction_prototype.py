3f9f1440ef192a49111b765775608c42252aa831
EmilHoejrup <emilhoejrup@gmail.com>
2023-12-04 11:56:15 +0100

    Simplify prediction_prototype plotting and fix one more bug from fitting to test data

--- prediction_prototype.py
+++ prediction_prototype.py
@@ -19,13 +19,22 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error, multilabel_confusion_matrix
 from pprint import pprint
-
+from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.neural_network import MLPClassifier
 
 data_df, data_ordinal_df, holdout_df = get_dfs()
+
+
+def classification_metrics(X, y, model, y_test, y_pred):
+    classification_report(y_test, y_pred)
+
+    print_confusion_matrix(y_test, y_pred, y)
+    plot_feature_importances(model, X)
+
+
 # %%
 # TRYING TO PREDICT TRACK POPULARITY BASED ON MUSICAL FEATURES
 X = data_ordinal_df
@@ -60,23 +69,17 @@ print(grid_search.best_params_)
 
 # %%
 # TRYING TO PREDICT GENRE BASED ON MUSICAL FEATURES
-# dropping 'year' since it is not a musical feature
-X = data_ordinal_df.drop('year', axis=1)
+# dropping 'year' since it is not a musical feature (also dropping 'mode' and 'key' since they do not make any difference in the result)
+X = data_ordinal_df.drop(['year', 'key', 'mode'], axis=1)
 y = data_df['playlist_genre']
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.33, random_state=42)
 
-dt_model = DecisionTreeClassifier()
-dt_model.fit(X_test, y_test)
-y_pred = dt_model.predict(X_test)
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
 # overraskende god performance
-# multilabel_confusion_matrix(y_test, y_pred)
-
-classification_report(y_test, y_pred)
-
-print_confusion_matrix(y_test, y_pred, y)
-# plotting the feature importance extracted from the dtree model
-plot_feature_importances(dt_model, X)
+classification_metrics(X, y, model, y_test, y_pred)
 
 
 # PREDICT DECADE
@@ -91,21 +94,17 @@ X = data_ordinal_df.drop(['year'], axis=1)
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.33, random_state=42)
 
-dt_model = DecisionTreeClassifier()
-dt_model.fit(X_train, y_train)
-y_pred = dt_model.predict(X_test)
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
 
-classification_report(y_test, y_pred)
-
-print_confusion_matrix(y_test, y_pred, y)
-# plotting the feature importance extracted from the dtree model
-plot_feature_importances(dt_model, X)
+classification_metrics(X, y, model, y_test, y_pred)
 
 
 # %%
-# PREDICTING POPULARITY BASED ON track_name
+# PREDICTING POPULARITY BASED ON track_name AND track_album_name
 
-# %%
+
 data_df['text'] = data_df['track_name'] + ' ' + data_df['track_album_name']
 X = data_df['text']
 y = data_df['playlist_genre']
@@ -134,4 +133,51 @@ ypred = pipe.predict(X_test)
 print(classification_report(y_test, ypred))
 # ConfusionMatrixDisplay.from_estimator(pipe, X_test, y_test)
 print_confusion_matrix(y_test, ypred, y)
+# %%
+
+# COMBINING TEXT AND MUSICAL FEATURES FOR GENRE PREDICTION
+
+
+X = data_ordinal_df
+X['text'] = data_df['track_name'] + ' ' + data_df['track_album_name']
+y = data_df['playlist_genre']
+# preprocessing steps for different columns
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('text', TfidfVectorizer(ngram_range=(1, 6)), 'text'),
+        # using numeric columns only
+        ('scaler', StandardScaler(), X.select_dtypes(include='number').columns)
+    ]
+)
+pipe = Pipeline([
+    ('preprocessor', preprocessor),
+    ('lr', LogisticRegression())
+])
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.33, random_state=42)
+
+
+pipe.fit(X_train, y_train)
+ypred = pipe.predict(X_test)
+print(classification_report(y_test, ypred))
+# ConfusionMatrixDisplay.from_estimator(pipe, X_test, y_test)
+print_confusion_matrix(y_test, ypred, y)
+# %%
+
+
+# PREDICTING SUBGENRE BASED ON MUSICAL FEATURES
+X = data_ordinal_df.drop(['year', 'key', 'mode'], axis=1)
+y = data_df['playlist_subgenre']
+X = X.drop('text', axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.33, random_state=42)
+
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+# overraskende god performance
+# multilabel_confusion_matrix(y_test, y_pred)
+classification_metrics(X, y, model, y_test, y_pred)
+
 # %%
